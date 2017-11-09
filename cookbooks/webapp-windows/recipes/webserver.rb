a0613ef1eb0c83c4e42aea4cf181c73e311b92b9
Jmcdermott7001 <jpm9rb@mail.umkc.edu>
2017-11-09 11:23:18 -0600

    Modified webserve.rb

--- cookbooks/webapp-windows/recipes/webserver.rb
+++ cookbooks/webapp-windows/recipes/webserver.rb
@@ -3,3 +3,35 @@
 # Recipe:: webserver
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#Enable the IIS role
+dsc_script 'Web-Server' do
+    code <<-EOH
+    WindowsFeature InstallWebServer
+    {
+        Name = "Web-Server"
+        Ensure = "Present"
+    }
+    EOH
+end
+
+#Install ASP.net 4.5
+dsc_script 'Web-Asp-Net45' do
+    code <<-EOH
+    WindowsFeature InstallAspDotNet45
+    {
+        Name = "Web-Asp-Net45"
+        Ensure = "Present"
+    }
+    EOH
+end
+
+#Install IIS Managmenet Console
+dsc_script 'Web-Mgmt-Console' do
+    code <<-EOH
+    WindowsFeature InstallIISConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
+    EOH
+end
