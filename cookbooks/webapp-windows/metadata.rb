37bf39a527cb94a9757515111501540763c4acc7
Jmcdermott7001 <jpm9rb@mail.umkc.edu>
2017-11-16 13:09:34 -0600

    Update webapp metadata.rb

--- cookbooks/webapp-windows/metadata.rb
+++ cookbooks/webapp-windows/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures webapp-windows'
 long_description 'Installs/Configures webapp-windows'
-version '0.1.1'
+version '0.1.2'
 
 depends 'sql_server', '~> 2.4.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
